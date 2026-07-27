2d484e014129ce7eef21890f388edd456068e500
Roberto-Araujo-98 <1684720@senati.pe>
2026-07-26 20:10:39 -0500

    build(tauri): actualizar tauri.conf.json y backend lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,14 @@
+#[tauri::command]
+fn resolver_en_rust(pantalla: String) -> Result<String, String> {
+    // Aquí invocaremos tu simulador
+    Ok(format!("Resultado Rust: {}", pantalla))
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        // Registramos tus comandos 
+        .invoke_handler(tauri::generate_handler![resolver_en_rust])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
